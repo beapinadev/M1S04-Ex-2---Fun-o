6c9895902b96ab39148f26b79b8b18974a0164ac
beapinadev <beatrizpinadev@gmail.com>
2024-02-22 20:20:21 -0300

    teste github

---
+++ script.js
@@ -0,0 +1,12 @@
+let numero = 12;
+
+function parOuImpar (){
+    if (numero % 2 == 0){
+        console.log("Este é um número par.")
+    }
+    else {
+        console.log("Este é um número ímpar.")
+    }
+}
+
+parOuImpar ()
